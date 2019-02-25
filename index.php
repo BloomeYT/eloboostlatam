ae69b0b157a1fd47d94ab20df1e07f2e5f1adb61
BloomeYT <38925000+BloomeYT@users.noreply.github.com>
2019-02-25 17:40:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
                                 </a> 
                                 <h3>CUENTAS</h3> 
                                 <p>Los precios varían desde 40 $USD hasta 80 $USD según el rangos (Oro a Diamante).</p>
-                                <a href="https://eloboostlatam.herokuapp.com/" class="servcies-more-link btn btn-primary">Saber más</a> 
+                                <a href="https://eloboostlatam-cuentas.herokuapp.com/" class="servcies-more-link btn btn-primary">Saber más</a> 
                             </div> 
                             <div class="col-sm-12 service box-primary"> 
                                 <a href="/servicios/maestr%C3%8Da-7" class="">
